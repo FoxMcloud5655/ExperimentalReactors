722ec0d0605a6dbdf9cb689f7911f6199ece243e
QuantumZizo <zayn.elborno@13bananas.com>
2023-09-28 19:10:16 -0600

    Edited Mod.toml

--- src/main/java/com/quantumzizo/experimentalreactors/ExperimentalReactors.java
+++ src/main/java/com/quantumzizo/experimentalreactors/ExperimentalReactors.java
@@ -1,8 +1,11 @@
 package com.quantumzizo.experimentalreactors;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("experimentalreactors")
 public class ExperimentalReactors {
-
+	public ExperimentalReactors() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
 }
